cb1598ee586c5dd08c109a05e9d4a671eb94ff98
kasiditmxisa-ux <kasiditmxisa@gmail.com>
2026-07-04 23:06:01 +0700

    User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -161,9 +161,9 @@
 #define TFT_MISO 19 // Matching T_DO
 #define TFT_MOSI 23 // Matching T_DIN
 #define TFT_SCLK 18 // Matching T_CLK
-#define TFT_CS   17  // Chip select control pin
-#define TFT_DC   16  // Data Command control pin
-#define TFT_RST   5  // Reset pin (could connect to RST pin)
+#define TFT_CS   15  // Chip select control pin
+#define TFT_DC   22  // Data Command control pin
+#define TFT_RST   4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 #define TFT_BL   32  // LED back-light (only for ST7789 with backlight control pin)
